8fc293d995171a8e11bbf11c8008140d7b508514
jeppsOptim <jepps@optim.aero>
2025-03-18 22:01:03 -0500

    Adding functionality to launch PX4 repo checked out on WSL side

--- initVehicleSIL.m
+++ initVehicleSIL.m
@@ -1,36 +1,38 @@
-% initVehicleSIL: This function checks to make sure the matlab environment is set up properly. This file checks if the 
+% initVehicleSIL: This function checks to make sure the matlab environment is set up properly. This file checks if the
 % user has the required toolboxes, support packages, and loads variable and bus definitions that are used in the
 % F-16.slx model.
 % ======================================================================================================================
 %                                                       INPUTS
 % ======================================================================================================================
-% opts.launchFullSIL:            Flag used to launch the full SIL, should not be used on first attempt of using SIL
+% opts.launchFullSIL:            Flag used to launch the full SIL, set to False if desire is to only to load workspace. 
+%                                NOTE: should not be used on first attempt of using SIL. 
 % opts.visualizationType:        The method that will be used to visualize the aircraft while SIL is running
-% opts.launchQGroundControl:     This is a flag telling the function that QGC is not already launched
-% opts.simHostIP:                 IP Address of users computer
+% opts.simHostIP:                IP Address of users computer
 % opts.vehicleType:              The plant model that will be used during the SIL simulation (currently can only be F-16)
 % opts.controllerType:           The flight controller used in the SIL simulation (currently can only be PX4)
-%
+% opts.PX4RepoPath               Path relative to optimAeroPX4SIL if PX4 repo is on windows side, or path relative to root wsl directory
+% opts.PX4InWSL                  If attempting to use the PX4 repo checked out on the wsl side, set this variable to true
 % ======================================================================================================================
 %                                                    EXAMPLE USAGE
 % ======================================================================================================================
-% initVehicleSIL("launchFullSIL", true, "visualizationType","FlightGear","simHostIP","10.0.0.200"): Launch full SIL sim and visualize vehicle using FlightGear and set IP
+% initVehicleSIL("launchFullSIL", true, "visualizationType","FlightGear","simHostIP","10.0.0.200","PX4InWSL",true): 
+% Launch full SIL sim, visualize vehicle using FlightGear, use the PX4 repo checked out on the WSL side and set IP 
 % address for PX4 connection.
-% initVehicleSIL("launchFullSIL",false,):             Run the intialization file only, this should be used before changing any models
+% initVehicleSIL("launchFullSIL",false):   Run the intialization file only, this should be used before changing any models
 function initVehicleSIL(opts)
 
-    arguments
-        opts.launchFullSIL        (1,1) logical = false        % input true if running the full SIL is desired
-        opts.vehicleType          (1,1) string  = "F-16"       % Currently F-16 is the only vehicle config
-        opts.visualizationType    (1,1) string  = "Matlab"     % Options "PassThrough", "FlightGear", or "Matlab"
-        opts.launchQGroundControl (1,1) logical = false        % If QGC is already launched set this to false (TODO: currently non-functional)
-        opts.simHostIP            (1,1) string  = "10.0.0.243" % Replace with your IP address
-        opts.controllerType       (1,1) string  = "PX4"        % Currently PX4 is the only controller that can be used
-        opts.PX4RepoPath          (1,1) string  = "PX4-Autopilot" % This is the path to the PX4 repo
-    end
-% Note: In future versions these will be arguments 
-vehicleParams.type                  = opts.vehicleType; 
-vehicleParams.controllerType        = opts.controllerType; 
+arguments
+    opts.launchFullSIL        (1,1) logical = false        
+    opts.vehicleType          (1,1) string  = "F-16"       % Currently F-16 is the only vehicle config
+    opts.visualizationType    (1,1) string  = "Matlab"     % Options "PassThrough", "FlightGear", or "Matlab"
+    opts.simHostIP            (1,1) string  = "10.0.0.243" % Replace with your IP address
+    opts.controllerType       (1,1) string  = "PX4"        % Currently PX4 is the only controller that can be used
+    opts.PX4RepoPath          (1,1) string  = "px4-autopilot"   
+    opts.PX4InWSL             (1,1) logical = false        
+end
+% Note: In future versions these will be arguments
+vehicleParams.type                  = opts.vehicleType;
+vehicleParams.controllerType        = opts.controllerType;
 visualizationType                   = opts.visualizationType;
 controllerType                      = opts.controllerType;
 vehicleType                         = opts.vehicleType;
@@ -38,14 +40,14 @@ vehicleType                         = opts.vehicleType;
 % check for required toolboxes, support packages, and MATLAB version
 % list is here: (https://www.mathworks.com/matlabcentral/answers/377731-how-do-features-from-license-correspond-to-names-from-ver#answer_300675)
 requiredTools ={'Aerospace_Blockset'
-                'Aerospace_Toolbox'
-                'Control_Toolbox'
-                'MATLAB_Coder'
-                'Real-Time_Workshop' %Simulink Coder
-                'RTW_Embedded_Coder' %Embedded Coder
-                'SIMULINK' 
-                'Simulink_Test'
-                'UAV_Toolbox'};              
+    'Aerospace_Toolbox'
+    'Control_Toolbox'
+    'MATLAB_Coder'
+    'Real-Time_Workshop' %Simulink Coder
+    'RTW_Embedded_Coder' %Embedded Coder
+    'SIMULINK'
+    'Simulink_Test'
+    'UAV_Toolbox'};
 
 requiredSupportPackages = {};
 requiredSupportPackages(1).Name = {'UAV Toolbox Support Package for PX4 Autopilots'};
@@ -68,15 +70,15 @@ else
     for ii = 1:numel(requiredSupportPackages)
         compareIdx = contains(supportPackagesInstalledNames, requiredSupportPackages(ii).Name);
         if ~any(compareIdx)
-           warning(['Please install ' char(requiredSupportPackages(ii).Name)])  
+            warning(['Please install ' char(requiredSupportPackages(ii).Name)])
         elseif ~strcmp(supportPackagesInstalledVersions(compareIdx), requiredSupportPackages(ii).Version)
             warning(['Please install v' requiredSupportPackages(ii).Version])
         end
     end
 end
 
-% initSIL.m Initializes path, load bus definitions, and sets model 
-% parameters for vehicleSIL.slx 
+% initSIL.m Initializes path, load bus definitions, and sets model
+% parameters for vehicleSIL.slx
 addpath(genpath('PX4-Autopilot'));
 addpath(genpath('environment'));
 addpath(genpath('sensors'));
@@ -116,15 +118,15 @@ if strcmp(vehicleParams.type, "F-16")
     vehicleParams.bRef_m = 30 * ft2m;
     vehicleParams.SRef_m2 = 27.8709;
     vehicleParams.cRef_m = 11.32 * ft2m;% Mean Aero Chord
-    vehicleParams.aircraftCg_m = [vehicleParams.cRef_m * 0.35; 0; 0]; 
+    vehicleParams.aircraftCg_m = [vehicleParams.cRef_m * 0.35; 0; 0];
     vehicleParams.refCG_m = [vehicleParams.cRef_m * 0.35; 0; 0]; % reference cg
-    aeroDataF16    
+    aeroDataF16
 end
 
 % Initial Conditions
 % set initial location to Juancho E. Yrausquin Airport
 referenceAltitude_m = 125 * ft2m;
-referenceLatitude_deg = 17.645927; 
+referenceLatitude_deg = 17.645927;
 referenceLongitude_deg = -63.222027;
 aircraftInitialPosInNED_m = [0, 0, -referenceAltitude_m];
 aircraftInitialVelInBody_mps = [0, 0, 0];
@@ -136,7 +138,7 @@ aircraftInitial.lat_deg = referenceLatitude_deg;
 aircraftInitial.lon_deg = referenceLongitude_deg;
 aircraftInitial.alt_m = aircraftInitialPosInNED_m(3);
 aircraftInitial.yawAngle_rad = aircraftInitialEuler_rad(3);
-terrainHeightNED_m = aircraftInitialPosInNED_m(3); 
+terrainHeightNED_m = aircraftInitialPosInNED_m(3);
 
 parameters.gps.vertPositionAccuracy_m = 40;
 parameters.gps.horzPositionAccuracy_m = 30;
@@ -201,6 +203,7 @@ end
 save('workspace')
 evalin("base", 'load workspace.mat')
 delete workspace.mat
+
 % Launch full SIL if requested
 if opts.launchFullSIL
     % Run flightgear if requested
@@ -210,28 +213,32 @@ if opts.launchFullSIL
     end
 
     % Open QGC
-    % Note: System currenlty freezing when running this from script
     disp('Launch QGroundControl manually.');
-    %pause;
-
 
     % Launch PX4
-    eval(strcat("cd ", opts.PX4RepoPath))
     opts.simHostIPVal = double(split(opts.simHostIP, '.'));
-    [status,cmdout] = system(sprintf('start wsl bash -c "export PX4_SIM_HOSTNAME=%d.%d.%d.%d && make px4_sitl_default optimAeroF16"',...
-        opts.simHostIPVal(1), opts.simHostIPVal(2), opts.simHostIPVal(3), opts.simHostIPVal(4)));
-    % Open simulink model
-    if isempty(cmdout)
-        % cmdout being empty means PX4 is running If it is not empty then most likely there's an error
-        cd(currLoc)
-        open VehicleSilSimulation.slx
-        sim VehicleSilSimulation
+    if ~opts.PX4InWSL
+        % Launch PX4-Autopilot that is checked out on the Windows side
+        eval(strcat("cd ", opts.PX4RepoPath))
+        opts.simHostIPVal = double(split(opts.simHostIP, '.'));
+        [~,cmdout] = system(sprintf('start wsl bash -c "export PX4_SIM_HOSTNAME=%d.%d.%d.%d && make px4_sitl_default optimAeroF16"',...
+            opts.simHostIPVal(1), opts.simHostIPVal(2), opts.simHostIPVal(3), opts.simHostIPVal(4)));
     else
-        warning(['If the repo path argument points to the WSL side, matlab cannot call the make command. PX4 must be' ...
-            ' built manually'])
-        error(cmdout)
-
+        % Launch PX4-Autopilot that is checked out on the WSL side
+        [~,cmdout] = system(sprintf('start wsl bash -c "cd ~/%s && export PX4_SIM_HOSTNAME=%d.%d.%d.%d && make px4_sitl_default optimAeroF16"',...
+            opts.PX4RepoPath, opts.simHostIPVal(1), opts.simHostIPVal(2), opts.simHostIPVal(3), opts.simHostIPVal(4)));
     end
+
+    if ~isempty(cmdout)
+        % Display system command output if system call fails
+        error(cmdout)
+    end
+
+    % Open simulink model
+    cd(currLoc)
+    open VehicleSilSimulation.slx
+    % Run simulink model
+    sim VehicleSilSimulation
 
 end
 
